59849a8d6716fbdacfc52fafe5aaa3c8e7c569f7
IvanPB16 <41024238+IvanPB16@users.noreply.github.com>
2019-02-20 18:31:56 -0600

    +ch update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express()
 
-app.get('/', (req,res) => res.send('Hola mundo'))
+app.get('/', (req,res) => res.send('Hola mundo, mi primera practica'))
 app.listen(3000, () => console.log('El servidor se ejecuta en http://localhost:3000'))
